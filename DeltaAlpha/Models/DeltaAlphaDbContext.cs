494a8743c6d475f91975b675d326723c38babf66
bruyer_k <kevyn.bruyere@epitech.eu>
2014-04-08 00:17:09 -0700

    A la recherche de la DLL perdue

--- DeltaAlpha/Models/DeltaAlphaDbContext.cs
+++ DeltaAlpha/Models/DeltaAlphaDbContext.cs
@@ -9,6 +9,10 @@ namespace DeltaAlpha.Models
 {
     public class DeltaAlphaDbContext : DbContext
     {
+        public DeltaAlphaDbContext() : base("DefaultConnection")
+        {
+        }
+
         public DbSet<PledgeClass> PledgeClasses { get; set; }
         public DbSet<Brother> Brothers { get; set; }
         public DbSet<Family> Families { get; set; }
